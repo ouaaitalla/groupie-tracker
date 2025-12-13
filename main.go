aa2b2869282bfd2552cd778d592d997a71f56fb9
houssam <housam@gmail.com>
2025-12-13 12:06:52 +0100

    v2

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
-	"text/template"
+	"strconv"
 )
 
 type Artist struct {
@@ -17,6 +18,8 @@ type Artist struct {
 	FirstAlbum   string   `json:"firstAlbum"`
 	Relations    string   `json:"relations"`
 }
+
+var allArtists []Artist
 
 func FetchArtists() ([]Artist, error) {
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
@@ -35,14 +38,53 @@ func FetchArtists() ([]Artist, error) {
 	return artists, err
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	artis, _ := FetchArtists()
-	tmpl, _ := template.ParseFiles("index.html")
-	tmpl.Execute(w, artis)
+func handlerArtist(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid artist ID", http.StatusBadRequest)
+		return
+	}
+
+	var artist Artist
+	for _, a := range allArtists {
+		if a.ID == id {
+			artist = a
+			break
+		}
+	}
+
+
+	tmpl, err := template.ParseFiles("articst.html")
+	if err != nil {
+		http.Error(w, "Failed to load template", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, artist)
+}
+
+func handlerHome(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, "Failed to load template", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, allArtists)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	var err error
+	allArtists, err = FetchArtists()
+	if err != nil {
+		fmt.Println("Error fetching artists:", err)
+		return
+	}
+
+	http.HandleFunc("/", handlerHome)
+	http.HandleFunc("/artist", handlerArtist)
+
 	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }

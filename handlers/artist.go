85e5ff2628110b0daa910a1d2a3fa1d1dad5064d
achent <chentaymane234@gmail.com>
2026-01-04 10:42:50 +0100

    .

--- handlers/artist.go
+++ handlers/artist.go
@@ -8,15 +8,7 @@ import (
 	"strings"
 )
 
-// Artist represents the structure of an artist
-type Artist struct {
-	ID           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-}
+
 
 // HandlerArtist serves the artist detail page
 func HandlerArtist(w http.ResponseWriter, r *http.Request) {
--- handlers/fetchaartist.go
+++ handlers/fetchaartist.go
@@ -4,7 +4,15 @@ import (
 	"encoding/json"
 	"net/http"
 )
-
+// Artist represents the structure of an artist
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+}
 // FetchARtists retrieves the list of artists from the public API
 func FetchArtists() ([]Artist, error) {
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
